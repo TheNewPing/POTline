a7c36f9da00836147a160c84fe9cded38b3f6a72
TheNewPing <edoardorodaro@gmail.com>
2024-11-15 18:37:14 +0100

    added cli options

--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
+from argparse import Namespace, ArgumentParser
 from potline import PotLine
 
 if __name__ == '__main__':
-    potline = PotLine('config.yaml', 1, True, True)
+    def parse_args() -> Namespace:
+        parser: ArgumentParser = ArgumentParser(description='Process some parameters.')
+        parser.add_argument('--config', type=str, required=True, help='Path to the config file')
+        parser.add_argument('--iterations', type=int, required=True, help='Number of iterations')
+        parser.add_argument('--inference', action='store_true', help='Enable inference benchmark')
+        parser.add_argument('--properties', action='store_true', help='Enable properties simulation')
+        return parser.parse_args()
+
+    args: Namespace = parse_args()
+    config_path: str = args.config
+    iterations: int = args.iterations
+    inference_flag: bool = args.inference
+    data_analysis_flag: bool = args.properties
+
+    potline = PotLine(config_path, iterations, inference_flag, data_analysis_flag)
     potline.run()
