802b1e2af497a577475fb72554eaa363c0228a75
TheNewPing <edoardorodaro@gmail.com>
2024-11-20 17:10:55 +0100

    timings to csv

--- src/potline/lammps_runner/template/run.sh
+++ src/potline/lammps_runner/template/run.sh
@@ -14,14 +14,10 @@ echo "finished"
 runtime1=$((mid_time-start_time))
 runtime2=$((end_time-mid_time))
 runtime3=$((runtime2-runtime1))
-steps_diff=$((max_steps-prerun_steps))
 
 # Write timings to a file
-timings_file="${out_path}/bench_timings.txt"
-echo "start_time: $start_time" > $timings_file
-echo "mid_time: $mid_time" >> $timings_file
-echo "end_time: $end_time" >> $timings_file
-echo "steps_diff: $steps_diff" >> $timings_file
-echo "time_diff: $runtime3" >> $timings_file
+timings_file="${out_path}/bench_timings.csv"
+echo "start_time,mid_time,end_time,prerun_steps,max_steps,time_diff" > $timings_file
+echo "$start_time,$mid_time,$end_time,${prerun_steps},${max_steps},$runtime3" >> $timings_file
 
 exit 0
