4f69cdf5477e12072591fd43202d314e8ee4f8b0
TheNewPing <edoardorodaro@gmail.com>
2024-12-11 17:47:25 +0100

    renamed config file

--- src/main.py
+++ src/main.py
@@ -7,21 +7,20 @@ from pathlib import Path
 
 from potline import PotLine
 
-if __name__ == '__main__':
-    def parse_args() -> Namespace:
-        """
-        Parse the command line arguments.
-        """
-        parser: ArgumentParser = ArgumentParser(description='Process some parameters.')
-        parser.add_argument('--config', type=str, default='src/data/config.hjson',
-                            help='Path to the config file')
-        parser.add_argument('--nohyper', action='store_false', help='Disable hyperparameter search')
-        parser.add_argument('--nodeep', action='store_false', help='Disable deep training')
-        parser.add_argument('--noconversion', action='store_false', help='Disable yace conversion')
-        parser.add_argument('--noinference', action='store_false', help='Disable inference benchmark')
-        parser.add_argument('--noproperties', action='store_false', help='Disable properties simulation')
-        return parser.parse_args()
+def parse_args() -> Namespace:
+    """
+    Parse the command line arguments.
+    """
+    parser: ArgumentParser = ArgumentParser(description='Process some parameters.')
+    parser.add_argument('--config', type=str, help='Path to the config file')
+    parser.add_argument('--nohyper', action='store_false', help='Disable hyperparameter search')
+    parser.add_argument('--nodeep', action='store_false', help='Disable deep training')
+    parser.add_argument('--noconversion', action='store_false', help='Disable yace conversion')
+    parser.add_argument('--noinference', action='store_false', help='Disable inference benchmark')
+    parser.add_argument('--noproperties', action='store_false', help='Disable properties simulation')
+    return parser.parse_args()
 
+if __name__ == '__main__':
     args: Namespace = parse_args()
 
     potline = PotLine(Path(args.config),
--- src/run.py
+++ src/run.py
@@ -2,30 +2,15 @@
 CLI script for running PotLine.
 """
 
-from argparse import Namespace, ArgumentParser
+from argparse import Namespace
 from pathlib import Path
 
 from potline.dispatcher import DispatcherFactory, JobType
 from potline.config_reader import ConfigReader
 
-def parse_args() -> Namespace:
-    """
-    Parse the command line arguments.
-    """
-    parser: ArgumentParser = ArgumentParser(description='Process some parameters.')
-    parser.add_argument('--config', type=str, default='src/data/config.hjson',
-                        help='Path to the config file')
-    parser.add_argument('--nohyper', action='store_false', help='Disable hyperparameter search')
-    parser.add_argument('--nodeep', action='store_false', help='Disable deep training')
-    parser.add_argument('--noconversion', action='store_false', help='Disable yace conversion')
-    parser.add_argument('--noinference', action='store_false', help='Disable inference benchmark')
-    parser.add_argument('--noproperties', action='store_false', help='Disable properties simulation')
-    return parser.parse_args()
+from main import parse_args
 
-def run() -> None:
-    """
-    Run the PotLine pipeline.
-    """
+if __name__ == '__main__':
     args: Namespace = parse_args()
     config_path: Path = Path(args.config).resolve()
     main_path: Path = Path('src/main.py').resolve()
@@ -43,6 +28,3 @@ def run() -> None:
     dispatcher = DispatcherFactory(JobType.MAIN.value, config.cluster).create_dispatcher(
         commands, config.sweep_path, config.model_name)
     dispatcher.dispatch()
-
-if __name__ == '__main__':
-    run()
